dc6ffe8330c65e3f4691c5b3ddc7cd28e3e9ff35
Stanzy-Developer <nnoupingnouping@gmail.com>
2024-12-26 16:24:59 +0100

    Resolve Google Auth problem and upgraded Nextjs to v15

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,59 +1,79 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import Form from "@components/form";
+import { Suspense } from "react";
 
-const EditPrompt = () => {
+import Form from "@/components/form";
+
+const UpdatePrompt = () => {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const [Submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({ prompt: "", tag: "" });
-  const promptId = searchParams.get("id");
 
-  useEffect(() => {
-    const getPromptDetails = async () => {
-      const res = await fetch(`/api/prompt/${promptId}`);
-      const data = await res.json();
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
-    };
-    if (promptId) getPromptDetails();
-  }, [promptId]);
+  const SearchParamsWrapper = () => {
+    const searchParams = useSearchParams();
+    const promptId = searchParams.get("id");
 
-  const updatePrompt = async (e) => {
-    e.preventDefault(); //Preventing the default behavior of the browser.
-    setSubmitting(true); // sets submitting to true, in order to sent a request to the server and launch the loading state
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+      prompt: "",
+      tag: "",
+    });
 
-    if (!promptId) return alert("Prompt ID not found");
-    try {
-      const res = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
-      });
-      if (res.ok) {
-        router.push("/");
+    useEffect(() => {
+      const getPromptDetails = async () => {
+        const response = await fetch(`/api/prompt/${promptId}`);
+        const data = await response.json();
+
+        setPost({
+          prompt: data.prompt,
+          tag: data.tag,
+        });
+      };
+
+      if (promptId) getPromptDetails();
+    }, [promptId]);
+
+    const updatePrompt = async (e) => {
+      e.preventDefault();
+      setSubmitting(true);
+
+      if (!promptId) return alert("Prompt ID not found");
+
+      try {
+        const response = await fetch(`/api/prompt/${promptId}`, {
+          method: "PATCH",
+          body: JSON.stringify({
+            prompt: post.prompt,
+            tag: post.tag,
+          }),
+        });
+
+        if (response.ok) {
+          router.push("/");
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setSubmitting(false);
       }
-    } catch (e) {
-      console.log("error");
-    } finally {
-      setSubmitting(false);
-    }
+    };
+
+    return (
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    );
   };
+
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      Submitting={Submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div className="text-black">Loading...</div>}>
+      <SearchParamsWrapper />
+    </Suspense>
   );
 };
 
-export default EditPrompt;
+export default UpdatePrompt;
